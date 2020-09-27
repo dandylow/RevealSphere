8d1137e7197131aeb1e0578fbafb7e3260bc9619
dandylow <64595351+dandylow@users.noreply.github.com>
2020-09-27 17:18:31 +0200

    Add files via upload

---
+++ SphereReveal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SphereReveal : MonoBehaviour
+{
+    //static radius of the sphere
+    public float radius = 6f;
+    //dynamic radius (changes with time)
+    public float rad;
+    //positive=sphere reveals, negative=sphere hides
+    public float softness = 0.05f;
+    
+    
+
+
+    
+
+    private void Start()
+    {
+        rad = radius;
+    }
+    
+    void Update()
+    {
+        
+        radius = radius- (rad/10)*Time.deltaTime;
+        Shader.SetGlobalVector("_GLOBALMaskPosition", transform.position);
+        Shader.SetGlobalFloat("_GLOBALMaskRadius", radius);
+        Shader.SetGlobalFloat("_GLOBALMaskSoftness", softness);
+       
+    }
+}
